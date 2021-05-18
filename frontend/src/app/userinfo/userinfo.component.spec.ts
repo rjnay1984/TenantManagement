8e6f4c7b5b6144b2aad1e97f19fb2d3f7191f0bd
RJ Nay <rjnay1984@gmail.com>
2021-05-18 15:49:40 -0500

    update spec

--- frontend/src/app/userinfo/userinfo.component.spec.ts
+++ frontend/src/app/userinfo/userinfo.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { of } from 'rxjs';
 
 import { UserinfoComponent } from './userinfo.component';
 
@@ -8,9 +11,10 @@ describe('UserinfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserinfoComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [UserinfoComponent],
+      providers: [{ provide: OidcSecurityService, useValue: of('test') }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- frontend/src/app/userinfo/userinfo.component.ts
+++ frontend/src/app/userinfo/userinfo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-userinfo',
@@ -7,10 +8,11 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
   styleUrls: ['./userinfo.component.scss']
 })
 export class UserinfoComponent implements OnInit {
+  userData$!: Observable<any>;
 
   constructor(public oidcSecurityService: OidcSecurityService) {}
 
   ngOnInit(): void {
+    this.userData$ = this.oidcSecurityService.userData$;
   }
-
 }
