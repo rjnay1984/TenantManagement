df4358cd497b7103d6b9faac1da42a69b028540b
RJ Nay <rjnay1984@gmail.com>
2021-05-28 15:04:06 -0500

    seed data

---
+++ backend/src/Identity/Data/DbInitializer.cs
@@ -0,0 +1,162 @@
+﻿using Identity.Models;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Identity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var adminRole = roleManager.FindByNameAsync("Admin").Result;
+            if (adminRole == null)
+            {
+                adminRole = new IdentityRole("Admin");
+
+                var result = roleManager.CreateAsync(adminRole);
+                result.Wait();
+                if (!result.IsCompletedSuccessfully)
+                {
+                    throw new Exception(result.Exception.Message);
+                }
+            }
+
+            var landlordRole = roleManager.FindByNameAsync("Landlord").Result;
+            if (landlordRole == null)
+            {
+                landlordRole = new IdentityRole("Landlord");
+
+                var result = roleManager.CreateAsync(landlordRole);
+                result.Wait();
+                if (!result.IsCompletedSuccessfully)
+                {
+                    throw new Exception(result.Exception.Message);
+                }
+            }
+
+            var tenantRole = roleManager.FindByNameAsync("Tenant").Result;
+            if (tenantRole == null)
+            {
+                tenantRole = new IdentityRole("Tenant");
+
+                var result = roleManager.CreateAsync(tenantRole);
+                result.Wait();
+                if (!result.IsCompletedSuccessfully)
+                {
+                    throw new Exception(result.Exception.Message);
+                }
+            }
+
+            var alice = userManager.FindByNameAsync("alice").Result;
+            if (alice == null)
+            {
+                alice = new ApplicationUser
+                {
+                    UserName = "AliceSmith@email.com",
+                    Email = "AliceSmith@email.com",
+                    FirstName = "Alice",
+                    LastName = "Smith",
+                    EmailConfirmed = true,
+                };
+
+                var result = userManager.CreateAsync(alice, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddToRolesAsync(alice, new[] { "Admin" }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddClaimsAsync(alice, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, $"{alice.FirstName} {alice.LastName}"),
+                            new Claim(JwtClaimTypes.GivenName, alice.FirstName),
+                            new Claim(JwtClaimTypes.FamilyName, alice.LastName),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                        }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+            }
+
+            var bob = userManager.FindByNameAsync("bob").Result;
+            if (bob == null)
+            {
+                bob = new ApplicationUser
+                {
+                    FirstName = "Bob",
+                    LastName = "Smith",
+                    UserName = "BobSmith@email.com",
+                    Email = "BobSmith@email.com",
+                    EmailConfirmed = true
+                };
+                var result = userManager.CreateAsync(bob, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddToRoleAsync(bob, "Landlord").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddClaimsAsync(bob, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, $"{bob.FirstName} {bob.LastName}"),
+                            new Claim(JwtClaimTypes.GivenName, bob.FirstName),
+                            new Claim(JwtClaimTypes.FamilyName, bob.LastName),
+                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                            new Claim("location", "somewhere")
+                        }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+            }
+
+            var aaron = userManager.FindByNameAsync("aaron").Result;
+            if (aaron == null)
+            {
+                aaron = new ApplicationUser
+                {
+                    FirstName = "Aaron",
+                    LastName = "Rodgers",
+                    UserName = "aaron@email.com",
+                    Email = "aaron@email.com",
+                    EmailConfirmed = true
+                };
+
+                var result = userManager.CreateAsync(aaron, "Pass123$").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddToRoleAsync(aaron, "Tenant").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userManager.AddClaimsAsync(aaron, new Claim[]{
+                            new Claim(JwtClaimTypes.Name, $"{aaron.FirstName} {aaron.LastName}"),
+                            new Claim(JwtClaimTypes.GivenName, aaron.FirstName),
+                            new Claim(JwtClaimTypes.FamilyName, aaron.LastName),
+                        }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+            }
+        }
+    }
+}
--- backend/src/Identity/Program.cs
+++ backend/src/Identity/Program.cs
@@ -1,11 +1,11 @@
+using Identity.Data;
+using Identity.Models;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Identity
 {
@@ -13,7 +13,32 @@ namespace Identity
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context, userManager, roleManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
