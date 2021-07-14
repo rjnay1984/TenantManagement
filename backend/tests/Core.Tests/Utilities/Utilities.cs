58b612961ee1d94ceb3e138a62ee6c953c2ea374
RJ <rjnay1984@gmail.com>
2021-07-14 15:47:57 -0500

    user repository unit tests (#10)

---
+++ backend/tests/Core.Tests/Utilities/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using Core.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Tests
+{
+    public class Utilities
+    {
+        public static DbContextOptions<IdentityContext> TestIdentityContextOptions()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+             var builder = new DbContextOptionsBuilder<IdentityContext>()
+                .UseInMemoryDatabase("Identity")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+    }
+}
