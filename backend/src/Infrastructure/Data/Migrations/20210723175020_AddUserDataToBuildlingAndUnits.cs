1dc2480e2eb99f7bb1d861ed36c50ad2684f0953
RJ <rjnay1984@gmail.com>
2021-07-23 13:02:02 -0500

    Add user data to building and unit models (#15)

---
+++ backend/src/Infrastructure/Data/Migrations/20210723175020_AddUserDataToBuildlingAndUnits.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class AddUserDataToBuildlingAndUnits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UnitNumber",
+                table: "Units",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "Tenants",
+                table: "Units",
+                type: "text[]",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Buildings",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Landlord",
+                table: "Buildings",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tenants",
+                table: "Units");
+
+            migrationBuilder.DropColumn(
+                name: "Landlord",
+                table: "Buildings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UnitNumber",
+                table: "Units",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Buildings",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
